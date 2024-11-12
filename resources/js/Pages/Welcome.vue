f7bda6b68c6575e55f2441116cc0223ce12d335a
Daniel Betancur <daniel.b@somosgentedigital.com>
2024-11-12 13:52:26 -0500

    feat: add filters user, bookings rooms

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -147,10 +147,10 @@ const scrollToResult = () => {
             </Link>
 
             <template v-else>
-                <Link :href="route('login')"
+                <!--<Link :href="route('login')"
                       class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
                     Ingresar
-                </Link>
+                </Link>-->
 
                 <!--<Link v-if="canRegister" :href="route('register')"
                       class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
